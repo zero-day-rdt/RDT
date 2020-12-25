ee8c8cee2c023fd2363b4b5d6dce281e02ac7174
Star-Sky-18 <1033690636@qq.com>
2020-12-25 16:51:13 +0800

    解决了warning

---
+++ single connection test/client.py
@@ -0,0 +1,43 @@
+from rdt import RDTSocket
+import time
+from difflib import Differ
+
+client = RDTSocket()
+client.connect(('127.0.0.1', 9999))
+
+data_count = 0
+echo = b''
+count = 3
+
+with open('alice.txt', 'r') as f:
+    data = f.read()
+    encoded = data.encode()
+    assert len(data)==len(encoded)
+
+start = time.perf_counter()
+for i in range(count): # send 'alice.txt' for count times
+    data_count+=len(data)
+    client.send(encoded)
+
+'''
+blocking send works but takes more time 
+'''
+
+while True:
+    reply = client.recv(2048)
+    echo += reply
+    print(reply)
+    if len(echo)==len(encoded)*count:
+        break
+client.close()
+
+'''
+make sure the following is reachable
+'''
+
+print(f'transmitted {data_count}bytes in {time.perf_counter()-start}s')
+print(f'tr = {data_count*2 / (time.perf_counter() - start) / 1000} KB/s\n')
+print(data*count == echo.decode())
+diff = Differ().compare((data*count).splitlines(keepends=True), echo.decode().splitlines(keepends=True))
+for line in diff:
+    assert line.startswith('  ') # check if data is correctly echoed
